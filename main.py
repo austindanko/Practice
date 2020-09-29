f0856e874c82c00c5e8c50872ca4ac9f44e63e17
Austin Danko <austin.danko@uconn.edu>
2020-09-28 20:55:51 -0400

    add initial code to github.

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import pynmrstar
-# This is a sample Python script.
+entry15000 = pynmrstar.Entry.from_database(15000)
+#Overall entry structure, remove # below
+#entry15000.print_tree()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#Creating inital saveframe.
+Save_Person = pynmrstar.Saveframe.from_scratch("Name", "Name") #Name being the tag within my initial saveframe, but why does it need to be quoted twice?
+print(Save_Person)
+#Now to add a loop for work
