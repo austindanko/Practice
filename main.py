b87fca1e3f9fc4a274ab1a875643ebb14adfa5a1
Austin Danko <austin.danko@uconn.edu>
2020-09-28 21:01:27 -0400

    add initial code to github.

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ Save_Person = pynmrstar.Saveframe.from_scratch("Name", "Name") #Name being the t
 print(Save_Person)
 #Now to add a loop for work
 Work = pynmrstar.Loop.from_scratch()
+
+#add tags into the loop
+Work.add_tag(['Work_history.Job_title', 'Work_history.Start', 'Work_history.End', 'Work_history.Responsiblity'])
 print(Work)
