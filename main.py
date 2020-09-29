740ab04799e926af81ea75b995b13542d71b33d8
Austin Danko <austin.danko@uconn.edu>
2020-09-29 12:03:12 -0400

    add initial code to github.

--- main.py
+++ main.py
@@ -14,4 +14,8 @@ Work.add_tag(['Work_history.Job_title', 'Work_history.Start', 'Work_history.End'
 ##print(Work)
 #add loop into saveframe
 Save_Person.add_loop(Work)
+#print(Save_Person)
+#Now that the loop is within the saveframe, add_tag() & add_data() to the saveframe & loop for Email assignment similarity
+Save_Person.add_tag("ZZZ", "YYY")
+Work.add_data(['n/a', '01-01-1979', '01-01-1979', "Programmed Systems"])
 print(Save_Person)
