1ea0029a34495e75968b14c83f219a7c3dd2ff1b
Austin Danko <austin.danko@uconn.edu>
2020-09-30 11:43:09 -0400

    add initial code to github.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ entry15000 = pynmrstar.Entry.from_database(15000)
 #entry15000.print_tree()
 
 #Creating inital saveframe.
-Save_Person = pynmrstar.Saveframe.from_scratch("Name", "Name") #Name being the tag within my initial saveframe, but why does it need to be quoted twice?
+#Saveframe (Name, Value)
+Save_Person = pynmrstar.Saveframe.from_scratch("Person", "Name")
 ##print(Save_Person)
 #Now to add a loop for work
 Work = pynmrstar.Loop.from_scratch()
@@ -16,6 +17,8 @@ Work.add_tag(['Work_history.Job_title', 'Work_history.Start', 'Work_history.End'
 Save_Person.add_loop(Work)
 #print(Save_Person)
 #Now that the loop is within the saveframe, add_tag() & add_data() to the saveframe & loop for Email assignment similarity
-Save_Person.add_tag("ZZZ", "YYY")
-Work.add_data(['n/a', '01-01-1979', '01-01-1979', "Programmed Systems"])
+Save_Person.add_tag("Fist_Name", "ZZZ")
+Save_Person.add_tag("Last_Name", "YYY")
+Work.add_data(['ZZZ', '01-01-1979', '01-01-1979', 'Programmed Systems'])
+Work.add_data(['YYY', '01-01-2000', '01-01-2000', 'Hacked the planet'])
 print(Save_Person)
