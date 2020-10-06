fb194b95fc5a61c391ee68dd9a4325ae245cfcec
Austin Danko <austin.danko@uconn.edu>
2020-10-06 10:42:25 -0400

    add initial code to github.

--- main.py
+++ main.py
@@ -5,20 +5,26 @@ entry15000 = pynmrstar.Entry.from_database(15000)
 
 #Creating inital saveframe.
 #Saveframe (Name, Value)
-Save_Person = pynmrstar.Saveframe.from_scratch("Person", "Name")
+save_person = pynmrstar.Saveframe.from_scratch("Person", "Name")
 ##print(Save_Person)
 #Now to add a loop for work
-Work = pynmrstar.Loop.from_scratch()
+work = pynmrstar.Loop.from_scratch()
 
 #add tags into the loop
-Work.add_tag(['Work_history.Job_title', 'Work_history.Start', 'Work_history.End', 'Work_history.Responsiblity'])
+work.add_tag(['Work_history.Job_title', 'Work_history.Start', 'Work_history.End', 'Work_history.Responsiblity'])
 ##print(Work)
 #add loop into saveframe
-Save_Person.add_loop(Work)
+save_person.add_loop(work)
 #print(Save_Person)
 #Now that the loop is within the saveframe, add_tag() & add_data() to the saveframe & loop for Email assignment similarity
-Save_Person.add_tag("Fist_Name", "ZZZ")
-Save_Person.add_tag("Last_Name", "YYY")
-Work.add_data(['ZZZ', '01-01-1979', '01-01-1979', 'Programmed Systems'])
-Work.add_data(['YYY', '01-01-2000', '01-01-2000', 'Hacked the planet'])
-print(Save_Person)
+save_person.add_tag("First_Name", "ZZZ")
+save_person.add_tag("Last_Name", "YYY")
+work.add_data(['ZZZ', '01-01-1979', '01-01-1979', 'Programmed Systems'])
+work.add_data(['YYY', '01-01-2000', '01-01-2000', 'Hacked the planet'])
+print(save_person)
+
+save_person.write_to_file("file_name.str")
+
+#pynmrstar.utils.iter_entries()
+
+#Fix capitalization, Send to File, Start new assignment
