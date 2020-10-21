b006237c95d216a8bb9433a0ed9965b3f9e66714
Austin Danko <austin.danko@uconn.edu>
2020-10-21 11:26:14 -0400

    Applying my dictionary function to my previous Assignment. BMRB API however is down at this point so output is unknown.

--- Assignment_3.py
+++ Assignment_3.py
@@ -5,3 +5,18 @@ exp_dict = {x:lst.count(x)
             for x in lst[:5]}
 print(exp_dict)
 
+import pynmrstar
+
+all_entries = pynmrstar.utils.iter_entries()
+
+experiments = set()
+
+for entry in all_entries:
+    value = entry.get_tag('_Experiment.Name')
+    experiments.update(value)
+
+exp_dict_2 = {x:experiments.count(x)
+              for x in experiments[:10]}
+print(exp_dict_2)
+
+
