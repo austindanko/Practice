2867f790cb471e7b0867e1d278cfa8802d942816
Austin Danko <austin.danko@uconn.edu>
2020-10-21 12:37:41 -0400

    Since BMRB API is currently down, Running my own experiment upon my own created list to see if a list -> set -> dictionary with a count would work. Conclusion: A set cannot have a count function run on it.

--- Assignment_3.py
+++ Assignment_3.py
@@ -3,20 +3,29 @@ lst = ["e1", "e1", "e1", "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "
 
 exp_dict = {x:lst.count(x)
             for x in lst[:5]}
-print(exp_dict)
+#print(exp_dict)
 
-import pynmrstar
+exp_set = set()
+for x in lst:
+    exp_set.update(lst)
+#print(exp_set)
 
-all_entries = pynmrstar.utils.iter_entries()
+exp_dict_3 = {x:exp_set.count(x)
+              for x in exp_set}
+print(exp_dict_3)
 
-experiments = set()
+#import pynmrstar
 
-for entry in all_entries:
-    value = entry.get_tag('_Experiment.Name')
-    experiments.update(value)
+#all_entries = pynmrstar.utils.iter_entries()
 
-exp_dict_2 = {x:experiments.count(x)
-              for x in experiments[:10]}
-print(exp_dict_2)
+#experiments = set()
+
+#for entry in all_entries:
+#    value = entry.get_tag('_Experiment.Name')
+#    experiments.update(value)
+
+#exp_dict_2 = {x:experiments.count(x)
+#              for x in experiments[:10]}
+#print(exp_dict_2)
 
 
