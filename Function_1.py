9b9f2d4e2a202d777d5d21266f403274cc5240c1
Austin Danko <austin.danko@uconn.edu>
2020-11-04 10:17:14 -0500

    Cleaned up code, showing types

--- Function_1.py
+++ Function_1.py
@@ -1,30 +1,39 @@
-from typing import Set
-
+from typing import Set, Dict, Iterable, List
+import csv
 import requests
 import pynmrstar
-
 import time
-time.sleep(.02)
-
 
 known_good_values: Set[str] = set(requests.get('https://api.bmrb.io/v2/enumerations/_Experiment.Name').json()['values'])
-lst_k_g_v = list(known_good_values)
+dict_k_g_v: Dict[str, bool] = {x: True for x in known_good_values}
 
-all_entries = pynmrstar.utils.iter_entries()
-experiments = set()
+
+
+all_entries: Iterable[pynmrstar.Entry] = pynmrstar.utils.iter_entries()
+experiments: Set[str] = set([])
 for entry in all_entries:
     value = entry.get_tag('_Experiment.Name')
     experiments.update(value)
-lst_experiments = list(experiments)
+    time.sleep(.03)
 
-odd_balls = []
+lst_experiments: List[str] = list(experiments)
+
+odd_balls: List[str] = []
 
 for x in lst_experiments:
-    if x not in lst_k_g_v:
+    if x not in dict_k_g_v:
         odd_balls.append(x)
 
-exp_dict = {x:odd_balls.count(x)
-          for x in odd_balls[:10]}
+exp_dict: Dict[str, bool] = {}
+
+for item in odd_balls:
+    if item not in exp_dict:
+        exp_dict[item] = 1
+    else:
+        exp_dict[item] += 1
+
+exp_dict: Dict[str, bool] = {x:odd_balls.count(x)
+          for x in odd_balls}
 srt_dict = sorted(exp_dict.items(), key=lambda x:
                   x[1], reverse=True)
 for x in srt_dict:
