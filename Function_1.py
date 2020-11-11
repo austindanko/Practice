44b3a5eb23fa974ccf9571c47cabf3397b678f3c
Austin Danko <austin.danko@uconn.edu>
2020-11-11 11:43:06 -0500

    Applied a CSV file to all_entries in pynmstar Applied a CSV file to found similarities between known good values and oddballs

--- Function_1.py
+++ Function_1.py
@@ -7,33 +7,29 @@ import time
 import strsimpy as strsimpy
 from strsimpy.jaro_winkler import JaroWinkler
 
-
-
 known_good_values: Set[str] = set(requests.get('https://api.bmrb.io/v2/enumerations/_Experiment.Name').json()['values'])
 dict_kgv: Dict[str, bool] = {x: True for x in known_good_values}
 
 if not os.path.exists('api.bmrb.csv'):
-
     all_entries: Iterable[pynmrstar.Entry] = pynmrstar.utils.iter_entries()
     experiments: Set[str] = set([])
+    for entry in all_entries:
+        value = entry.get_tag('_Experiment.Name')
+        experiments.update(value)
+        time.sleep(.03)
 
-for entry in all_entries:
-    value = entry.get_tag('_Experiment.Name')
-    experiments.update(value)
-    time.sleep(.03)
-lst_experiments: List[str] = list(experiments)
+    lst_experiments: List[str] = list(experiments)
 
-with open('api.bmrb.csv', 'w') as f:
-    w = csv.writer(f)
-    w.writerows(lst_experiments.items())
+    with open('api.bmrb.csv', 'w') as file:
+        write = csv.writer(file)
+        write.writerows(lst_experiments)
 
-with open('api_bmrb.csv') as f:
-    r = csv.reader(f)
-    experiments_csv = {row for row in r}
+with open('api.bmrb.csv') as file:
+    experiments_csv = csv.reader(file)
 
 odd_balls: List[str] = []
 
-for x in lst_experiments:
+for x in file:
     if x not in dict_kgv:
         odd_balls.append(x)
 
@@ -44,8 +40,8 @@ x[1], reverse=True)
 
 jaro = JaroWinkler()
 
-for x, y in zip(dict_kgv, odd_balls):
+for x, y in zip(dict_kgv, srt_dict):
     if jaro.similarity(x, y) <= .5:
-        print(x, y)
-    else:
-        print("No Match")
+        with open(similarity.csv) as file2:
+            write = csv.writer(file2)
+            write.writerows()
--- Just_Testing.py
+++ Just_Testing.py
@@ -9,7 +9,7 @@ from strsimpy.jaro_winkler import JaroWinkler
 
 lst1 = ('abc', 'def', 'ghi', 'jkl', 'mno', 'pqr', 'stu', 'vwx', 'yz')
 lst2 = ('abc', 'xxx', 'xxx', 'jkl', 'xxx', 'pqq', 'stu', 'vwv', 'yz')
-
+outsiders = ()
 jaro = JaroWinkler()
 
 for x, y in zip(lst1, lst2):
@@ -19,15 +19,15 @@ for x, y in zip(lst1, lst2):
         print("No Match")
 
 
-    with open('api.bmrb.csv', 'w') as f:
-        w = csv.writer(f)
-        w.writerows($$$$$.items())
+#    with open('api.bmrb.csv', 'w') as f:
+#        w = csv.writer(f)
+#        w.writerows($$$$$.items())
+#
+#with open('api_bmrb.csv') as f:
+#    r = csv.reader(f)
+#    $$$$$ = {row[0]: row[1] for row in r}
 
-with open('api_bmrb.csv') as f:
-    r = csv.reader(f)
-    $$$$$ = {row[0]: row[1] for row in r}
 
-
-
+#if not os.path.exists('api.bmrb.csv'):
 
 
