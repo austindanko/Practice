5d52a1bb1106a3696ab2d042d512c6e044e93b24
Austin Danko <austin.danko@uconn.edu>
2020-12-29 00:12:07 -0500

    Informative lines of code for a users input

--- Function_2.py
+++ Function_2.py
@@ -6,7 +6,8 @@ letters = ('a', 'b', 'c', 'd', 'e')
 print("Welcome to BMRB's data organization")
 start = input("To begin data organization type start: ")
 if start == 'start':
-    for number, letter in enumerate(letters):
-        print(number, letter)
+    print(f'The acceptable options for {letters[:1]} are:')
+    for number, letter in enumerate(letters[1:]):
+        print(f"{number, letter}")
 
 
