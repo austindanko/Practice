cae639285c63f50e3be9f9ce0c3fa44ba547661d
Austin Danko <austin.danko@uconn.edu>
2020-12-29 20:51:57 -0500

    Clean up, towards instructing myself on next steps

--- Function_2.py
+++ Function_2.py
@@ -1,13 +1,25 @@
 
+alt_options = ('rewrite', 'skip')
+csvfile = ('obscurity', 'KGV1', 'KGV2', 'KGV3') + alt_options
+#for actual csv file have code skip over integers, to create a list similar to above^
 
 
-letters = ('a', 'b', 'c', 'd', 'e')
+
+
+
 
 print("Welcome to BMRB's data organization")
 start = input("To begin data organization type start: ")
 if start == 'start':
-    print(f'The acceptable options for {letters[:1]} are:')
-    for number, letter in enumerate(letters[1:]):
+    print(f'The acceptable options for {csvfile[:1]} are:')
+    for number, letter in enumerate(csvfile[1:]):
         print(f"{number, letter}")
+    #user input to choose 0-4
+    #if 0-2 are selected, add to new/updated csv file, print off next row to be analyzed
+    #if 3 selected, user input then add to the updated csv file, print off next row to be analyzed
+    #if skipped, send row to the end of the column, print off next row to be analyzed
+    #if end of csv/column reached, end
+    #if user input 'end', end
+
 
 
