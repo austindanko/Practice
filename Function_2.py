c73545e02eee10e10859b362544202ceb8e95d2c
Austin Danko <austin.danko@uconn.edu>
2021-01-04 20:09:16 -0500

    Quick change, incorporating a for loop to cycle through rows of the csv file for data comprehension.

--- Function_2.py
+++ Function_2.py
@@ -15,25 +15,26 @@ with open (f'jaro_sim.csv', 'r') as file:
     print("Welcome to BMRB's data organization")
     start = input("To begin data organization type start: ")
     if start == 'start':
-        print(f'The acceptable options for {rows[0][0]} are:')
-        for number, string in enumerate((rows[0][1:] + alt_options), start=1):
-            print(f"{number, string}")
-        choice = input("Your chosen option is?: ")
-        if choice == '1':
-            new_csv.append(rows[1])
-            print(new_csv)
-        elif choice == '2':
-            new_csv.append(rows[2])
-            print(new_csv)
-        elif choice == '3':
-            new_csv.append(rows[3])
-            print(new_csv)
-        elif choice == '4':
-            rewrite = input("What would you like to name this experiment?: ")
-            new_csv.append(rewrite)
-            print(new_csv)
-        elif choice == '5':
-            pass
+        for row in rows:
+            print(f'The acceptable options for {row[0]} are:')
+            for number, string in enumerate((row[1:] + alt_options), start=1):
+                print(f"{number, string}")
+            choice = input("Your chosen option is?: ")
+            if choice == '1':
+                new_csv.append(row[1])
+                print(new_csv)
+            elif choice == '2':
+                new_csv.append(row[2])
+                print(new_csv)
+            elif choice == '3':
+                new_csv.append(row[3])
+                print(new_csv)
+            elif choice == '4':
+                rewrite = input("What would you like to name this experiment?: ")
+                new_csv.append(rewrite)
+                print(new_csv)
+            elif choice == '5':
+                pass
     #user input to choose 1-5
     #if 1-3 are selected, add to new/updated csv file, print off next row to be analyzed
     #if 4 selected, user input then add to the updated csv file, print off next row to be analyzed
