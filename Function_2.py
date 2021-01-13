2ac3a865f057c7ac644a8af3805612c2089ee632
Austin Danko <austin.danko@uconn.edu>
2021-01-13 01:59:23 -0500

    Disregard last commit & push Added another alt option, 'end', to quit out of the program removed print statements refactored new_csv to user_input applied .upper command to [Y/N]

--- Function_2.py
+++ Function_2.py
@@ -1,17 +1,9 @@
 import csv
-import pandas
 
-
-alt_options = ['rewrite', 'skip']
+alt_options = ['rewrite', 'skip', 'end']
 rows = []
 values_only = []
-new_csv = []
-
-dataframe = pandas.read_csv("jaro_sim.csv")
-dataframe["new_column"] = ""
-dataframe.to_csv("jaro_sim.csv", index=False)
-#for each new column created, length of values_only line must also increase
-
+user_input = []
 
 with open (f'jaro_sim.csv', 'r') as file:
     csv_reader = csv.reader(file)
@@ -27,14 +19,14 @@ with open (f'jaro_sim.csv', 'r') as file:
                 values_only.append(value)
 #Removal of numeric values from rows
     values_only = [[values_only[value], values_only[value + 1], values_only[value + 2], values_only[value + 3]]
-                   for value in range(0, len(values_only), 5)]
+                   for value in range(0, len(values_only), 400)]
 #Conversion of list back into tuples
 
     print("Welcome to BMRB's data organization")
     start = input("To begin data organization type start: ")
     if start == 'start':
         row_num = input("Would you like to view the amount of faulty experiments? [Y/N]: ")
-        if row_num == 'Y':
+        if row_num.upper() == 'Y':
             print("Total no. of rows: %d" % (csv_reader.line_num))
         else:
             pass
@@ -44,21 +36,17 @@ with open (f'jaro_sim.csv', 'r') as file:
                 print(f"{number, string}")
             choice = input("Your chosen option is?: ")
             if choice == '1':
-                new_csv.append(value[1])
-                print(new_csv)
+                user_input.append(value[1])
             elif choice == '2':
-                new_csv.append(value[2])
-                print(new_csv)
+                user_input.append(value[2])
             elif choice == '3':
-                new_csv.append(value[3])
-                print(new_csv)
+                user_input.append(value[3])
             elif choice == '4':
                 rewrite = input("What would you like to name this experiment?: ")
-                new_csv.append(rewrite)
-                print(new_csv)
+                user_input.append(rewrite)
             elif choice == '5':
                 pass
-
-
+            elif choice == '6':
+                quit()
 
 
