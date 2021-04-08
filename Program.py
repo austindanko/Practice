bf7a9f2b2b35b2d648aae18c66181c194ea7e714
Austin Danko <austin.danko@uconn.edu>
2021-04-08 13:00:59 -0400

    Organized for easy read through, Fully functioning for data remediation.

--- Program.py
+++ Program.py
@@ -6,16 +6,6 @@ import requests
 import pynmrstar
 from strsimpy.jaro_winkler import JaroWinkler
 
-known_good_values: Set[str] = set(requests.get('https://api.bmrb.io/v2/enumerations/_Experiment.Name').json()['values'])
-
-
-def kgv_transfer():
-    with open('new_kgv.csv', 'r') as file_new_kgv:
-        csv_reader = csv.reader(file_new_kgv)
-        for row in csv_reader:
-            known_good_values.update(row)
-
-
 def get_choice(values_only: List[str]) -> [str]:
     alt_options = ['new_KGV', 'rewrite', 'skip', 'end']
     count = 0
@@ -38,14 +28,12 @@ def get_choice(values_only: List[str]) -> [str]:
                 with open('new_kgv.csv', 'a') as file_new_kgv:
                     csv_writer = csv.writer(file_new_kgv)
                     csv_writer.writerow([values_only[0]])
-                kgv_transfer()
                 return values_only[0]
             else:
                 rewrite = input("What would you like to name this experiment?: ")
                 with open('new_kgv.csv', 'a') as file_new_kgv:
                     csv_writer = csv.writer(file_new_kgv)
                     csv_writer.writerow([rewrite])
-                kgv_transfer()
                 return rewrite
         elif choice == '5':
             rewrite = input("What would you like to name this experiment?: ")
@@ -74,65 +62,80 @@ def get_values(original_row: [Union[str, float]]) -> None:
 
 def obscure(experiments: List[str]) -> List[str]:
     obscurities: List[str] = []
+    current_known_good_values = get_known_good_values()
     for obscurity in experiments:
-        if obscurity not in known_good_values and obscurity not in pynmrstar.definitions.NULL_VALUES:
+        if obscurity not in current_known_good_values and obscurity not in pynmrstar.definitions.NULL_VALUES:
             obscurities.append(obscurity)
     return obscurities
 
 
-def experiment_sim(obscurities: List[str]) -> None:
-    jaro = JaroWinkler().similarity
-    obscurity_count: Dict[str, bool] = {x: obscurities.count(x) for x in obscurities}
-    print('Experiment similarities...')
-    with open('jaro_sim.csv', 'a') as file_jaro_sim, open('jaro_sim_orphan.csv', 'a') as file_jaro_sim_orphan:
-        jaro_sim_writer = csv.writer(file_jaro_sim)
-        jaro_sim_orphan_writer = csv.writer(file_jaro_sim_orphan)
-        for obscurity in set(sorted(obscurities)):
-            sim_val: List[Tuple[str, float]] = []
-            for good_value in known_good_values:
-                similarity_value: float = jaro(obscurity, good_value)
-                sim_val.append((good_value, similarity_value))
-                sim_val.sort(reverse=True, key=operator.itemgetter(1))
-            topthree: List[Union[str, float, int]] = []
-            topthree.extend([x for y in sim_val[:3] for x in y])
-            output = [obscurity, obscurity_count[obscurity]] + topthree
-            if obscurity_count[obscurity] <= 10:
-                jaro_sim_orphan_writer.writerow(output)
-            else:
-                jaro_sim_writer.writerow(output)
+def create_or_update_matches(obscurities: List[str]) -> None:
+    if not os.path.exists('jaro_sim.csv'):
+        print('Running experiment similarities...')
+        jaro = JaroWinkler().similarity
+        obscurity_count: Dict[str, bool] = {x: obscurities.count(x) for x in obscurities}
+        with open('jaro_sim.csv', 'a') as file_jaro_sim, \
+                open('jaro_sim_orphan.csv', 'a') as file_jaro_sim_orphan:
+            jaro_csv_writer = csv.writer(file_jaro_sim)
+            orphan_csv_writer = csv.writer(file_jaro_sim_orphan)
+            fields: List[str] = ['Obscurity', 'Count', 'KGV1', 'Similarity1',
+                                 'KGV2', 'Similarity2', 'KGV3', 'Similarity3']
+            jaro_csv_writer.writerow(fields)
+            orphan_csv_writer.writerow(fields)
+
+            current_known_good_values = get_known_good_values()
+            for obscurity in set(sorted(obscurities)):
+                sim_val: List[Tuple[str, float]] = []
+                for good_value in current_known_good_values:
+                    similarity_value: float = jaro(obscurity, good_value)
+                    sim_val.append((good_value, similarity_value))
+                    sim_val.sort(reverse=True, key=operator.itemgetter(1))
+                topthree: List[Union[str, float, int]] = []
+                topthree.extend([x for y in sim_val[:3] for x in y])
+                output = [obscurity, obscurity_count[obscurity]] + topthree
+                if obscurity_count[obscurity] <= 10:
+                    orphan_csv_writer.writerow(output)
+                else:
+                    jaro_csv_writer.writerow(output)
+    else:
+        print('Simulation file already exists.')
+        return
 
 
-def existence() -> List[str]:
-    pathway: List[str] = ['api', 'jaro']
-    experiments: List[str] = []
-    for path in pathway:
-        if path == 'api':
-            if os.path.exists('api.bmrb.csv'):
-                with open('api.bmrb.csv', 'r') as file_api:
-                    api_csv_reader = csv.reader(file_api)
-                    experiments: List[str] = next(api_csv_reader)
-            else:
-                all_entries: Iterable[pynmrstar.Entry] = pynmrstar.utils.iter_entries()
-                print('Running api.bmrb...')
-                for entry in all_entries:
-                    value = entry.get_tag('_Experiment.Name')
-                    experiments.extend(value)
-                with open('api.bmrb.csv', 'w') as file_api:
-                    api_csv_writer = csv.writer(file_api)
-                    api_csv_writer.writerow(experiments)
-        elif path == 'jaro':
-            if os.path.exists('jaro_sim.csv'):
-                break
-            else:
-                with open('jaro_sim.csv', 'a') as file_jaro_sim, \
-                        open('jaro_sim_orphan.csv', 'a') as file_jaro_sim_orphan:
-                    jaro_csv_writer = csv.writer(file_jaro_sim)
-                    orphan_csv_writer = csv.writer(file_jaro_sim_orphan)
-                    fields: List[str] = ['Obscurity', 'Count', 'KGV1', 'Similarity1',
-                                         'KGV2', 'Similarity2', 'KGV3', 'Similarity3']
-                    jaro_csv_writer.writerow(fields)
-                    orphan_csv_writer.writerow(fields)
-    return experiments
+def get_and_cache_experiments() -> List[str]:
+    api_sourced_experiments: List[str] = []
+
+    if not os.path.exists('api.bmrb.csv'):
+        print("Regenerating experiments from API...")
+        all_entries: Iterable[pynmrstar.Entry] = pynmrstar.utils.iter_entries()
+        print('Running api.bmrb...')
+        for entry in all_entries:
+            value = entry.get_tag('_Experiment.Name')
+            api_sourced_experiments.extend(value)
+        with open('api.bmrb.csv', 'w') as file_api:
+            api_csv_writer = csv.writer(file_api)
+            api_csv_writer.writerow(api_sourced_experiments)
+        return api_sourced_experiments
+    else:
+        print('Reading experiments from local cache...')
+        with open('api.bmrb.csv', 'r') as file_api:
+            api_csv_reader = csv.reader(file_api)
+            return next(api_csv_reader)
+
+
+def get_known_good_values() -> Set[str]:
+    print('Loading known good values from API and local file...')
+    api_good_values: Set[str] = set(
+        requests.get('https://api.bmrb.io/v2/enumerations/_Experiment.Name').json()['values'])
+
+    if os.path.exists('new_kgv.csv'):
+        with open('new_kgv.csv', 'r') as file_new_kgv:
+            csv_reader = csv.reader(file_new_kgv)
+            local_good_values = set([_ for _ in csv_reader])
+            all_good_values = api_good_values | local_good_values
+            return all_good_values
+    else:
+        return api_good_values
 
 
 def user_input():
@@ -157,8 +160,8 @@ def user_input():
             os.rename('temp_file.csv', 'jaro_sim.csv')
 
 
-existence()
-experiment_sim(obscure(existence()))
-
-
+experiments = get_and_cache_experiments()
+obscurities = obscure(experiments)
+create_or_update_matches(obscurities)
 user_input()
+
